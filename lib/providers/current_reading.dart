1c1e1a5fc2c1a0b522b2fd70df752bbfe7d8411b
Anxcye <anxcye@outlook.com>
2025-10-20 16:39:46 +0800

    fix: add logging for finish reading state in CurrentReading class

--- lib/providers/current_reading.dart
+++ lib/providers/current_reading.dart
@@ -1,4 +1,5 @@
 import 'package:anx_reader/models/current_reading_state.dart';
+import 'package:anx_reader/utils/log/common.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'current_reading.g.dart';
@@ -37,6 +38,7 @@ class CurrentReading extends _$CurrentReading {
 
   void finish() {
     state = state.copyWith(isReading: false);
+    AnxLog.info('CurrentReading: finish reading ${state.book?.title}');
   }
 
   bool get isReading => state.isReading;
