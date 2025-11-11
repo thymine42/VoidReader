afef2a3be13552995c474aa1d2bbd4733a793b64
Anxcye <anxcye@outlook.com>
2025-11-11 20:49:25 +0800

    feat: reorganize dashboard tile structure and implement new tile files for better modularity

--- lib/widgets/statistic/dashboard_tiles/dashboard_tile_registry.dart
+++ lib/widgets/statistic/dashboard_tiles/dashboard_tile_registry.dart
@@ -1,9 +1,9 @@
 import 'package:anx_reader/models/statistics_dashboard_tile.dart';
 import 'package:anx_reader/widgets/statistic/dashboard_tiles/dashboard_tile_base.dart';
-import 'package:anx_reader/widgets/statistic/dashboard_tiles/library_totals_tile.dart';
-import 'package:anx_reader/widgets/statistic/dashboard_tiles/period_summary_tile.dart';
-import 'package:anx_reader/widgets/statistic/dashboard_tiles/top_book_tile.dart';
-import 'package:anx_reader/widgets/statistic/dashboard_tiles/total_time_tile.dart';
+import 'package:anx_reader/widgets/statistic/dashboard_tiles/tiles/library_totals_tile.dart';
+import 'package:anx_reader/widgets/statistic/dashboard_tiles/tiles/period_summary_tile.dart';
+import 'package:anx_reader/widgets/statistic/dashboard_tiles/tiles/top_book_tile.dart';
+import 'package:anx_reader/widgets/statistic/dashboard_tiles/tiles/total_time_tile.dart';
 
 const Map<StatisticsDashboardTileType, StatisticsDashboardTileBase>
     dashboardTileRegistry = {
