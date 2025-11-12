c07161d12924de06f51da070423dedb20b36db2f
Anxcye <anxcye@outlook.com>
2025-11-12 21:14:10 +0800

    feat: refactor TopBookTile to improve layout and add book cover display

--- lib/widgets/statistic/dashboard_tiles/tiles/top_book_tile.dart
+++ lib/widgets/statistic/dashboard_tiles/tiles/top_book_tile.dart
@@ -1,6 +1,8 @@
 import 'package:anx_reader/models/statistics_dashboard_tile.dart';
 import 'package:anx_reader/providers/statistic_data.dart';
 import 'package:anx_reader/utils/date/convert_seconds.dart';
+import 'package:anx_reader/widgets/bookshelf/book_cover.dart';
+import 'package:anx_reader/widgets/common/async_skeleton_wrapper.dart';
 import 'package:anx_reader/widgets/statistic/dashboard_tiles/dashboard_tile_base.dart';
 import 'package:anx_reader/widgets/statistic/dashboard_tiles/dashboard_tile_metadata.dart';
 import 'package:anx_reader/widgets/tips/statistic_tips.dart';
@@ -19,44 +21,77 @@ class TopBookTile extends StatisticsDashboardTileBase {
         ));
 
   @override
+  Widget buildCorner(BuildContext context, WidgetRef ref) {
+    return cornerIcon(context, Icons.favorite);
+  }
+
+  @override
   Widget buildContent(
     BuildContext context,
     WidgetRef ref,
   ) {
-    final textTheme = Theme.of(context).textTheme;
-    final statisticData = ref.watch(statisticDataProvider);
-    return statisticData.when(
-      data: (data) {
+    return AsyncSkeletonWrapper(
+      asyncValue: ref.watch(statisticDataProvider),
+      builder: (data) {
         if (data.bookReadingTime.isEmpty) {
           return Center(child: FittedBox(child: StatisticsTips()));
         }
         final entry = data.bookReadingTime.first;
         final book = entry.keys.first;
         final seconds = entry.values.first;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+
+        final TextStyle bookTitleStyle = const TextStyle(
+          fontSize: 20,
+          fontFamily: 'SourceHanSerif',
+          fontWeight: FontWeight.bold,
+          overflow: TextOverflow.ellipsis,
+        );
+        final TextStyle bookAuthorStyle = const TextStyle(
+          fontSize: 12,
+          color: Colors.grey,
+          overflow: TextOverflow.ellipsis,
+        );
+        final TextStyle bookReadingTimeStyle = const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        );
+
+        return Row(
           children: [
-            Text(metadata.title, style: textTheme.titleMedium),
-            const SizedBox(height: 12),
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              title: Text(
-                book.title,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              subtitle: Text(
-                book.author,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-              trailing: Text(convertSeconds(seconds)),
+            BookCover(
+              book: book,
+              width: 120,
+              radius: 10,
+            ),
+            const SizedBox(width: 15),
+            Flexible(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(book.title, style: bookTitleStyle),
+                    const SizedBox(height: 5),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Text(book.author, style: bookAuthorStyle),
+                        ),
+                        Text(
+                            // getReadingTime(context),
+                            convertSeconds(seconds),
+                            textAlign: TextAlign.end,
+                            style: bookReadingTimeStyle),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [Expanded(child: Text('Graph'))],
+                    ),
+                  ]),
             ),
           ],
         );
       },
-      loading: () => const Center(child: CircularProgressIndicator()),
-      error: (error, _) => Text('$error'),
     );
   }
 }
